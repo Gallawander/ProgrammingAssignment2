12479fda0ed6b72f7df1715193c337fc3850a6a8
Gallawander <mondrej@centrum.sk>
2020-07-16 14:42:20 +0200

    Computation of an inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+### These functions take a matrix and return its inverted form. ###
 
-## Write a short comment describing this function
+## Makes a cachable matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(orig.m = matrix()) {
+  
+  # Initialization of the inverse property
+  inv.m <- NULL
+  
+  # Process to set the original matrix
+  set <- function(m){
+    orig.m <<- m
+    inv.m <<- NULL
+  }
+  
+  # Process to get the original matrix
+  get <- function() orig.m
+  
+  # Process to set the inverted matrix
+  set.inv <- function(solve) inv.m <<- solve
+  
+  # Process to get the inverted matrix
+  get.inv <- function() inv.m
+  
+  list(set = set,
+       get = get,
+       set.inv = set.inv,
+       get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the cachable matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cache.m, ...) {
+  inv.m <- cache.m$get.inv()
+  
+  #Checks for the cachable matrix
+  if(!is.null(inv.m)){
+    message("Getting inverted matrix")
+    return(inv.m)
+  }
+  
+  # Computes inverted matrix from data
+  data <- cache.m$get()
+  inv.m<-solve(data, ...)
+  cache.m$set.inv(inv.m)
+  inv.m
 }
-
-# cloning test
